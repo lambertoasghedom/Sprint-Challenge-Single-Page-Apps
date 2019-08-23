619e1d7b46cc60fcd88aa445a9ddcbb68b6309ac
Lamberto <asghedom.lamberto@gmail.com>
2019-08-23 13:41:45 -0700

    rendered welcome page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
     <main>
       <Header />
       <TabNav />
-      <CharacterList />
+      <WelcomePage />
+      {/* <CharacterList /> */}
     </main>
   );
 }
