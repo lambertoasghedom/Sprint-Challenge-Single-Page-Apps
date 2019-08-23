c56a8c0f753545cb6dda1b0c86648c49922efadc
Lamberto <asghedom.lamberto@gmail.com>
2019-08-23 12:19:12 -0700

    styled cards with semantic-ui

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Card, Icon, Image } from 'semantic-ui-react'
 import axios from 'axios';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -10,14 +12,46 @@ export default function CharacterList() {
     axios
       .get('https://rickandmortyapi.com/api/character/')
       .then(data => setCharacters(data.data.results))
+      .catch(err => `${err} 🔫🔫🔫🔫🔫`)
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map((char) => {
+        return (
+          <div key={char.id}>
+            <Card>
+              <Image src={char.image} alt={char.name} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>{char.name}</Card.Header>
+                <Card.Meta>{char.species} {char.status}</Card.Meta>
+                <Card.Description>
+                  Location: {char.location.name}
+                </Card.Description>
+                <Card.Description>
+                  Origin: {char.origin.name}
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    Episodes
+                  </a>
+                </Card.Content>
+            </Card>
+          </div>
+        )
+      })}
     </section>
   );
 }
 
 
+{/* <div key={char.id}>
+  <img src={char.image} alt=""/>
+  <h2>{char.name}</h2>
+  <p>{char.species} {char.status}</p>
+  <p>{char.location.name}</p>
+  <p>{char.origin.name}</p>
+</div> */}
