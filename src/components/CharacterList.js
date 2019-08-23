faadb604d70108c43736971179d0e2a63cbacae5
Lamberto <asghedom.lamberto@gmail.com>
2019-08-23 10:41:23 -0700

    semantic ui tabs

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [ characters, setCharacters ] = useState([])
+  console.log(characters)
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,7 +5,13 @@ import { NavLink } from "react-router-dom";
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
+const panes = [
+  { menuItem: 'Home Page', render: () => <Tab.Pane></Tab.Pane> },
+  { menuItem: 'Characters', render: () => <Tab.Pane></Tab.Pane> },
+  { menuItem: 'Locations', render: () => <Tab.Pane></Tab.Pane> },
+  { menuItem: 'Episodes', render: () => <Tab.Pane></Tab.Pane> }
+]
 
-};
+const TabExampleBasic = () => <Tab panes={panes} />
 
+export default TabExampleBasic
