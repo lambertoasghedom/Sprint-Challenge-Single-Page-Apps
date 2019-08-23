00b17dadc810ddf3a4ee688bdc1bdbde60443dc6
Lamberto <asghedom.lamberto@gmail.com>
2019-08-23 14:36:44 -0700

    rendered location component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import CharacterList from "./components/CharacterList.js";
 import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import LocationCard from "./components/LocationCard.js";
 
 
 export default function App() {
+
+  const [location, setLocation] = useState([])
+  console.log(location)
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/location/')
+      .then(data => setLocation(data.data.results))
+  }, [])
+
   return (
     <main>
       <Header />
       <TabNav />
       <WelcomePage />
       {/* <CharacterList /> */}
+      <LocationCard location={location}/>
     </main>
   );
 }
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({ location }) {
+  
+  // console.log(location.residents)
+  // let counter = 0;
+  // // let numbers = location.residents.map((local) => {
+  // //   counter += 1
+  // // })
+  
+  return (
+    <div>
+      {location.map((location) => {
+        return (
+          <Card
+            href='#card-example-link-card'
+            header={location.name}
+            meta={location.type}
+            meta={location.dimension}
+            description=""
+          />
+        )
+      })}
+    </div>
+  );
 }
